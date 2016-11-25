85b697f796eaa313d26bea52dea9ee5a37112b88
Robert <viniolli@gmail.com>
2016-11-25 19:48:35 +0100

    Rename Microservice to proper name

--- src/main/scala/rwalerow/app/ForumMicroservice.scala
+++ src/main/scala/rwalerow/app/ForumMicroservice.scala
@@ -1,15 +1,12 @@
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpResponse
-import akka.http.scaladsl.model.StatusCodes._
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
-import akka.http.scaladsl.server.Directives._
 import rwalerow.rest.Routes
 import rwalerow.utils.{ConfigurationImpl, PersistenceModuleImpl}
 
-object FirstMicroservice extends App {
+object ForumMicroservice extends App {
   implicit val system = ActorSystem()
   implicit val executor = system.dispatcher
   implicit val materializer = ActorMaterializer()
